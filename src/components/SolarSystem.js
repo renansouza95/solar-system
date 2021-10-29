db4940de5910714bb6f99e5db3b1ae3a99f052d1
Renan Souza <s.renan@outlook.com>
2021-10-29 16:47:35 -0300

    o data-testid de SolarSystem estava escrito errado

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -4,7 +4,7 @@ import Title from './Title';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testeid="solar-system">
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
       </div>
     );
