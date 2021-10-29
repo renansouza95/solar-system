9d1a596e509dacdcd736801ebc2dedba5ef7836b
Renan Souza <s.renan@outlook.com>
2021-10-29 15:49:17 -0300

    Componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1><strong>Sistema Solar</strong></h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
