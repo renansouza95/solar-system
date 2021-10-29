8514ac3d159cdf273ae24a6e6e77d54257f5652a
Renan Souza <s.renan@outlook.com>
2021-10-29 16:13:04 -0300

    Componente SolaySystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testeid="solar-system">
+        <p>Isso eh uma DIV</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
